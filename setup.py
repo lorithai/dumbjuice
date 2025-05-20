95f970019e46d891b756a94fefb2060c981944bd
lorithai <lorithai@gmail.com>
2025-05-20 11:17:30 +0200

    rename

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     entry_points={
         'console_scripts': [
             'dumbjuice-build = dumbjuice.build:build',
-            'create_ico = dumbjuice.create_ico_entry:main'
+            'dumbjuice-create_ico = dumbjuice.create_ico_entry:main'
         ],
     },
 )
